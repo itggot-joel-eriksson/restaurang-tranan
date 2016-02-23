a6db2683a07f8a306ed24121efd30085f6b2ef85
itggot-joel-eriksson <joel.eriksson3@itggot.se>
2016-02-23 13:20:36 +0100

    push to origin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
+    coffee = require("gulp-coffee"),
+    webserver = require("gulp-webserver"),
     autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("webserver", function() {
+    gulp.src("site")
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: "index.html"
+    }));
+});
 
 gulp.task("sass", function () {
     gulp.src("source/sass/*.sass")
@@ -15,17 +26,22 @@ gulp.task("sass", function () {
 gulp.task("autoprefix", function () {
     gulp.src("site/css/*.css")
         .pipe(autoprefixer({
-            browsers: ["last 3 versions"],
+            browsers: ["last 5 versions"],
             cascade: false
         }))
         .pipe(gulp.dest("site/css"));
 });
 
+gulp.task("coffee", function () {
+    gulp.src("source/coffee/*.coffee")
+        .pipe(coffee())
+        .pipe(gulp.dest("site/js"));
+});
+
 gulp.task("watch", function() {
     gulp.watch("source/sass/*.sass", ["sass"]);
+    gulp.watch("source/coffee/*.coffee", ["coffee"]);
     gulp.watch("site/css/*.css", ["autoprefix"]);
 });
 
-gulp.task("default", ["sass", "autoprefix", "watch"], function() {
-
-});
+gulp.task("default", ["webserver", "sass", "coffee", "watch"]);
